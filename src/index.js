2e84c1ed53d6ab9ee3a824a7a22019e8be273fd2
Collin Nehemia <collin.nehemia@ucglobalprograms.org>
2022-09-13 12:39:21 +0200

    Refracted mediator template

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ const express = require("express");
 const privateConfig = require('./config/private-config.json');
 const db = require('./models');
 const app = express();
-const PORT = privateConfig.appConfig.PORT
-const { getQueryParameters } = require('./openhim/initialize');
-
-//openHIM
-// getQueryParameters();
 
 app.all('*', async (req, res) => {
   // Starts when a new request is triggered by the polling channel
@@ -14,24 +9,13 @@ app.all('*', async (req, res) => {
     `\n${ new Date().toUTCString('en-GB', { timeZone: 'UTC' }) }  - `,
     `DHIS 2 <=> PTracker Mediator has received a new request. \n`
   );
-
-  
-  pushData.pushODKData().then(result => {
-    try {
-      res.json('PTracker Data from SQL Server.')
-    } catch (error) {
-        console.error(`Error sending data to PTracker: ${error}`)
-    }
-  }) 
 });
-
-
 
 //Server PORT
 db.sequelize.sync({}).then((req) => {
   app.listen(privateConfig.appConfig.PORT, (err) => {
       if (err) console.log(`Error: ${err}`)
-      console.log(`${privateConfig.appConfig.mediatorName}  listening on port ${privateConfig.appConfig.PORT}...  \n`);
+      console.log(`${privateConfig.appConfig.Name}  listening on port ${privateConfig.appConfig.PORT}...  \n`);
   });
 }).then(() => {
   console.log(`Succesfully connected to '${privateConfig.development.database}' database...  \n`)
