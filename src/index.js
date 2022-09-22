b98da80dbf4b4f35abd95fe3f35a9aeae7795512
Carmen <>
2022-09-22 14:24:16 +0200

    NMA-617, connected database file mediator

--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ const express = require("express");
 const privateConfig = require('./config/private-config.json');
 const db = require('./models');
 const app = express();
-const pTracker = require('./toCsv/jSonToCsv')
+const mediData = require('./toCsv/jSonToCsv')
 const {getQueryParameters }= require('./openhim/initialize');
 
 app.all('*', async (req, res) => {
   // Starts when a new request is triggered by the polling channel
   console.log(`\n---------------------------------------------------------------------------------`,
     `\n${ new Date().toUTCString('en-GB', { timeZone: 'UTC' }) }  - `,
-    `DHIS 2 <=> PTracker Mediator has received a new request. \n`
+    `DHIS 2 <=> Datatbase File Mediator has received a new request. \n`
   );
+  //
+  mediData.getPtrackerData();
 });
+
+//openhim 
 getQueryParameters();
-pTracker.getPtrackerData();
+
 
 //Server PORT
 db.sequelize.sync({}).then((req) => {
