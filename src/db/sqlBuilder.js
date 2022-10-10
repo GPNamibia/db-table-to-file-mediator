cbaf2b58783e2494263fdf1566d1201f29933f4c
Collin Nehemia <collin.nehemia@ucglobalprograms.org>
2022-10-10 14:38:02 +0200

    NMA-617(Updated where clause on period column)

--- src/db/sqlBuilder.js
+++ src/db/sqlBuilder.js
@@ -1,9 +1,19 @@
+// upsert record into MYSQL
+var Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+
 //A function that reads the table 
 async function readData(model) {
-    const foundItems = await model.findAll({attributes: {exclude: ['id']},},{ where: { period: '202210' } });
-    return foundItems;
-  }
+  const foundItems = await model.findAll({
+    where: {
+      period: {
+        [Op.substring]: '2022'
+      }
+    }
+  });
+  return foundItems;
+}
 
-  module.exports = {
-    readData,
-  };
+module.exports = {
+  readData,
+};
