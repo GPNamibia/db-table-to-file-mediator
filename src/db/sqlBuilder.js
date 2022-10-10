57ab93e9345f13ae55d0139b34a0042edb051138
Collin Nehemia <collin.nehemia@ucglobalprograms.org>
2022-10-10 15:54:27 +0200

    NMA-617(Added current year function)

--- src/db/sqlBuilder.js
+++ src/db/sqlBuilder.js
@@ -1,13 +1,15 @@
 // upsert record into MYSQL
 var Sequelize = require("sequelize");
 const Op = Sequelize.Op;
+const today = new Date()
+const currentYear = today.getFullYear()
 
 //A function that reads the table 
 async function readData(model) {
   const foundItems = await model.findAll({
     where: {
       period: {
-        [Op.substring]: '2022'
+        [Op.substring]: currentYear
       }
     }
   });
